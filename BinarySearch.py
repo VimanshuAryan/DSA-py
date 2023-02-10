faf82f746435ec45c11f310c1ca8b7cc677942bc
Vimanshu Aryan <vimanshuaryan@gmail.com>
2023-02-10 16:10:56 +0530

    binary search

--- BinarySearch.py
+++ BinarySearch.py
@@ -14,3 +14,25 @@ def search(nums, target):
         else:
             return m
     return -1
+
+nums = [-1,0,3,5,9,12]
+target = 9
+print(search(nums,target))
+
+def binary(nums,target):
+    l,r = 0, len(nums)-1
+    target= target
+    def recursion(nums, target,l,r):
+        m = (l+r)//2
+        if l > r:
+            return -1
+        elif nums[m] == target:
+            return m
+        elif nums[m] > target:
+            return recursion(nums,target,l,m-1)
+        elif nums[m] < target:
+            return recursion(nums,target,m+1,r)
+    return recursion(nums, target,l,r)
+print(binary(nums,target))
+
+    
