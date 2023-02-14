a190ef1c9d46a6c720d217bea5b2da00eedfd060
Vimanshu Aryan <vimanshuaryan@gmail.com>
2023-02-14 18:25:21 +0530

    koko

---
+++ koko.py
@@ -0,0 +1,23 @@
+#875 Koko eating bananas
+
+def minEatingSpeed(piles, h):
+    import math
+    l,r = 1,max(piles)
+    res = r
+
+    while l <= r:
+        k = (l+r)//2
+        hours = 0
+        for p in piles:
+            hours += math.ceil(p/k)
+        if hours <= h:
+            res = min(k,res)
+            r = k-1
+        else:
+            l = k+1
+    return res
+
+piles = [3,6,7,11]
+h = 8
+
+print(minEatingSpeed(piles,h))
