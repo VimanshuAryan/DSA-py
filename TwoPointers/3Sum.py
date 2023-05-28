1f65aa7a357907624a6ce3e9d38c2664137f218a
Vimanshu Aryan <vimanshuaryan@gmail.com>
2023-05-28 15:44:30 +0530

    28-05-23

--- TwoPointers/3Sum.py
+++ TwoPointers/3Sum.py
@@ -7,10 +7,15 @@ def sum(nums):
     res = []
 
     for i, a in enumerate(nums):
+
         # l and r need to be defined locally and not globally
         l,r = i+1, len(nums) - 1
+
+        # if curr value a is same the prev value a then continue with the next iteration of the loop
+        # because we don't want duplicates
         if i > 0 and nums[i-1] == a:
             continue
+
         while l<r:
             threeSum = a + nums[l] + nums[r]
             if threeSum > 0:
@@ -23,6 +28,7 @@ def sum(nums):
                 res.append([a, nums[l], nums[r]])
                 l += 1
                 print('perfect')
+                # we don't want duplicate 2 sum solution so if the value after shifting the ptr is same, we shift it again.
                 while nums[l] == nums[l-1] and l<r:
                     l+=1
     return res
